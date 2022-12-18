fb9a01ece113456782cfc4c02360d0b38bef6512
Ray Yoon <jaehoonyoon222@gmail.com>
2022-12-18 12:36:50 -0800

    updating queries

--- evstations.sql
+++ evstations.sql
@@ -10,19 +10,41 @@ CREATE TABLE stations (
     zip INT(5),
     access VARCHAR,
     ev_network VARCHAR,
-    connector_type VARCHAR,
+    conn_type VARCHAR,
     lat FLOAT,
     lng FLOAT,
     open_date DATE,
+    open_year INT(4),
+    open_month INT(2),
+    open_day INT(2),
+    CHADEMO INT(1),
+    J1772 INT(1),
+    J1772COMBO INT(1),
+    NEMA1450 INT(1),
+    NEMA515 INT(1),
+    NEMA520 INT(1),
+    TESLA INT(1)
 );
 
--- View table
+-- View entire table
 SELECT *
 FROM stations;
 
+-- Select state specific stations
 SELECT *
 FROM stations
 WHERE state = 'CA';
 
+-- Select station address
+SELECT station_name, address, city, state, zip
+FROM stations;
+
+-- Select stations and its coordinates
 SELECT station_name, lat, lng
 FROM stations;
+
+-- Select stations opened after a specific year
+SELECT station_name
+FROM stations
+WHERE open_year > 2010
+ORDER BY open_year;
